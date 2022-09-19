75eda17d2e59c2303b28acbb0cda526f0856c61f
Sophie E. Hill <43281937+sophieehill@users.noreply.github.com>
2022-09-19 13:02:28 +0100

    Create team-logos.R

---
+++ data/team-logos.R
@@ -0,0 +1,35 @@
+team_logos <-
+  structure(
+    list(
+      team = c(
+        "Arsenal",
+        "Manchester United",
+        "Aston Villa",
+        "Liverpool",
+        "Tottenham Hotspur",
+        "West Ham United",
+        "Manchester City",
+        "Chelsea",
+        "Leicester City",
+        "Everton",
+        "Brighton & Hove Albion",
+        "Reading"
+      ),
+      team = c(
+        "https://upload.wikimedia.org/wikipedia/en/5/53/Arsenal_FC.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/1200px-Manchester_United_FC_crest.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Aston_Villa_FC_crest_%282016%29.svg/1200px-Aston_Villa_FC_crest_%282016%29.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/1200px-Tottenham_Hotspur.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/West_Ham_United_FC_logo.svg/1200px-West_Ham_United_FC_logo.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/1200px-Manchester_City_FC_badge.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1200px-Chelsea_FC.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/Leicester_City_crest.svg/1200px-Leicester_City_crest.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/7/7c/Everton_FC_logo.svg/1200px-Everton_FC_logo.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/f/fd/Brighton_%26_Hove_Albion_logo.svg/1200px-Brighton_%26_Hove_Albion_logo.svg.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/Reading_FC.svg/1200px-Reading_FC.svg.png"
+      )
+    ),
+    row.names = c(NA,-12L),
+    class = c("tbl_df", "tbl", "data.frame")
+  )
