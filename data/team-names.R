fdbdd6abcf655b2e19325845895c38b8716278ba
Sophie E. Hill <43281937+sophieehill@users.noreply.github.com>
2022-09-19 12:47:38 +0100

    Create team-names.R

---
+++ data/team-names.R
@@ -0,0 +1,49 @@
+team_names <-
+  structure(
+    list(
+      team = c(
+        "Arsenal",
+        "Manchester United",
+        "Aston Villa",
+        "Liverpool",
+        "Tottenham Hotspur",
+        "West Ham United",
+        "Manchester City",
+        "Chelsea",
+        "Leicester City",
+        "Everton",
+        "Brighton & Hove Albion",
+        "Reading"
+      ),
+      guardian_name = c(
+        "Arsenal Women",
+        "Manchester Utd Women",
+        "Aston Villa Women",
+        "Liverpool FC Women",
+        "Tottenham Hotspur Women",
+        "West Ham Women",
+        "Man City Women",
+        "Chelsea Women",
+        "Leicester Women",
+        "Everton Women",
+        "Brighton & Hove Albion Women",
+        "Reading Women"
+      ),
+      short_name = c(
+        "ARS",
+        "MUN",
+        "AVL",
+        "LIV",
+        "TOT",
+        "WHU",
+        "MCI",
+        "CHE",
+        "LEI",
+        "EVE",
+        "BHA",
+        "REA"
+      )
+    ),
+    row.names = c(NA,-12L),
+    class = c("tbl_df", "tbl", "data.frame")
+  )
