94221672b0a2e7a9ea6b7103b3038fa2e0718085
Sophie E. Hill <43281937+sophieehill@users.noreply.github.com>
2022-09-19 12:38:46 +0100

    Create team-colors.R

---
+++ data/team-colors.R
@@ -0,0 +1,49 @@
+team_colors <-
+  structure(
+    list(
+      team = c(
+        "Arsenal",
+        "Manchester United",
+        "Aston Villa",
+        "Liverpool",
+        "Tottenham Hotspur",
+        "West Ham United",
+        "Manchester City",
+        "Chelsea",
+        "Leicester City",
+        "Everton",
+        "Brighton & Hove Albion",
+        "Reading"
+      ),
+      color_hex = c(
+        "#EF0107",
+        "#DA291C",
+        "#670E36",
+        "#C8102E",
+        "#132257",
+        "#7A263A",
+        "#6CABDD",
+        "#034694",
+        "#003090",
+        "#003399",
+        "#0057B8",
+        "#004494"
+      ),
+      color_main = c(
+        "Red",
+        "Red",
+        "Red",
+        "Red",
+        "Blue",
+        "Red",
+        "Blue",
+        "Blue",
+        "Blue",
+        "Blue",
+        "Blue",
+        "Blue"
+      )
+    ),
+    row.names = c(NA,-12L),
+    class = c("tbl_df", "tbl", "data.frame")
+  )
